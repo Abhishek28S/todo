33a4649f8cc839cc422130d957e86ab8fb9b73fb
Abhishek28S <as8489126@gmail.com>
2023-11-06 14:48:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const express = require("express");
 const app = express();
+require("./DB/connection");
 const cors = require("cors");
-const http = require("http");
+const http = require('http');
 
-const roomRouter = require("./route/room");
+const router = require("./route/route")
 
 app.use(cors());
 app.use(express.json());
-app.use("/room", roomRouter);
+app.use(router);
 
-const hostname = "0.0.0.0";
+const hostname = '0.0.0.0';
 
 const httpServer = http.createServer(app);
-httpServer.listen(8080, hostname, function () {
+httpServer.listen(8080, hostname, function() {
   const port = httpServer.address().port;
-  console.log("HTTP server running on port", port);
+  console.log('HTTP server running on port', port);
 });
