fa71bea47c1a6a58f1cb39a116de24a7527f0156
Abhishek28S <as8489126@gmail.com>
2023-11-06 14:38:53 +0530

    Create model.js

---
+++ model/model.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const bookSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  summary: {
+    type: String,
+    required: true,
+  },
+});
+
+const bookModel = new mongoose.model("bookSchema", bookSchema);
+module.exports = bookModel;
